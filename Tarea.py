0501c731a326798b08423d848cff6b444dbae586
NaomiD03 <91295116+NaomiD03@users.noreply.github.com>
2021-10-17 10:55:46 -0500

    Add files via upload

---
+++ Tarea.py
@@ -0,0 +1,14 @@
+d = {
+    "Omar":[19,1.80],
+    "Luisa":[10,1.40],
+    "Sebastian":[20,1.90],
+    "Alexander":[20,1.95],
+    "Alice":[17,1.60],
+    "Rachel":[25,1.75],
+    "Amelie":[30,1.70],
+    "Maria":[15,1.58],
+    "Cindy":[22,1.62]}
+
+d["Stefan"] = "24","1.80"
+del (d["Omar"])
+print(d)
